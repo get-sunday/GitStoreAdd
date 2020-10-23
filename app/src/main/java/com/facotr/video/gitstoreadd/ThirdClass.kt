7edaf32bc5d15eee5582411563888bd8639a0799
“lixin” <510131752@qq.com>
2020-10-23 13:54:58 +0800

    第三个页面提交

---
+++ app/src/main/java/com/facotr/video/gitstoreadd/ThirdClass.kt
@@ -0,0 +1,14 @@
+package com.facotr.video.gitstoreadd
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ *
+ *@author: lixin
+ *@time: 2020/10/23
+ */
+class ThirdClass : AppCompatActivity() {
+
+
+}
