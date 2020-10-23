977cd4f94ac2ab21a24cf7231c2fdd23f9825773
“lixin” <510131752@qq.com>
2020-10-23 13:46:20 +0800

    登录页面提交

---
+++ app/src/main/java/com/facotr/video/gitstoreadd/ScondActivity.kt
@@ -0,0 +1,18 @@
+package com.facotr.video.gitstoreadd
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ *
+ *@author: lixin
+ *@time: 2020/10/23
+ */
+class ScondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
